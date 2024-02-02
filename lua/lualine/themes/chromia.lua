4c34755a0d67847d4249fa69b7eef70822cec748
David Eriksson <david.eriksson@chromaway.com>
2024-02-02 15:36:22 +0100

    fix(lualine): proper groups

--- lua/lualine/themes/chromia.lua
+++ lua/lualine/themes/chromia.lua
@@ -3,39 +3,39 @@ local C = require "chromia.colors"
 local chromia = {}
 
 chromia.normal = {
-    a = { bg = C.orange, fg = C.black0, gui = "bold" },
+    a = { bg = C.orange, fg = C.black1, gui = "bold" },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.dark, fg = C.fg },
 }
 
 chromia.insert = {
-    a = { bg = C.green, fg = C.black0, gui = "bold" },
+    a = { bg = C.diff_add, fg = C.black1, gui = "bold" },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.dark, fg = C.fg },
 }
 
 chromia.command = {
-    a = { bg = C.cyan.bright, fg = C.black0, gui = "bold" },
+    a = { bg = C.context, fg = C.black1, gui = "bold" },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.dark, fg = C.fg },
 }
 
 chromia.visual = {
-    a = { bg = C.red.bright, fg = C.black0, gui = "bold" },
+    a = { bg = C.red_err, fg = C.black1, gui = "bold" },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.dark, fg = C.fg },
 }
 
 chromia.replace = {
-    a = { bg = C.magenta.bright, fg = C.black0, gui = "bold" },
+    a = { bg = C.boolean, fg = C.black1, gui = "bold" },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.dark, fg = C.fg },
 }
 
 chromia.terminal = {
-    a = { bg = C.blue2, fg = C.black0, gui = "bold" },
+    a = { bg = C.blue_type, fg = C.black1, gui = "bold" },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.dark, fg = C.fg },
 }
 
 chromia.inactive = {
